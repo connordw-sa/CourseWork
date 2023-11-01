71b2d70c079e7a4a09d6b224c3f402a7546d6e6b
Connor Wehmeyer <114667779+connordw-sa@users.noreply.github.com>
2023-11-01 15:36:22 +0200

    .

--- frontend/unit_1/w3-js+dom/day11/script.js
+++ frontend/unit_1/w3-js+dom/day11/script.js
@@ -1,19 +1,78 @@
-// EX7: Write a function to change the h1 text to something else
-let header = document.getElementsByTagName("h1");
-function changeHeader() {
-  header.addEventListener("click", () => {
-    header.innerText = "Smartphone Shop"
-      ? (header.innerText = "Shop Smartphone")
-      : "Smartphone Shop";
-  });
+function rcg() {
+  return "#" + (((1 << 24) * Math.random()) | 0).toString(16);
 }
 
+// EX7: Write a function to change the h1 text to something else
+
+function changeHeader() {
+  document.getElementsByTagName("h1")[0].addEventListener("click", function () {
+    this.innerText =
+      this.innerText == "Smartphone Shop"
+        ? "Shop Smartphone"
+        : "Smartphone Shop";
+  });
+}
+changeHeader();
 // EX8: Write a function to change the page background color
+
+function changePageColor() {
+  document
+    .getElementsByTagName("body")[0]
+    .addEventListener("click", function () {
+      this.style.backgroundColor = rcg();
+    });
+}
+changePageColor();
 
 // EX9: Write a function to change the footer address with a fake one
 
+function changeFooter() {
+  document
+    .getElementsByTagName("footer")[0]
+    .addEventListener("click", function () {
+      this.innerText =
+        this.innerText == "123 Street, Cape Town, Smartphone Shop"
+          ? "Click to change it back"
+          : "123 Street, Cape Town, Smartphone Shop";
+    });
+}
+
+changeFooter();
+
 // EX10: Write a function to add a CSS class to every Amazon link
+
+function addClass() {
+  let links = document.getElementsByTagName("a");
+  for (let link of links) {
+    link.classList.add("aClass");
+  }
+}
+addClass();
 
 // EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
 
+function toggleImage() {
+  let images = document.getElementsByTagName("img");
+  for (let img of images) {
+    img.addEventListener("click", function () {
+      img.classList.contains("imgClass")
+        ? img.classList.remove("imgClass")
+        : img.classList.add("imgClass");
+    });
+  }
+}
+
+toggleImage();
+
 // EX12: Write a function to color the price of the products in a different one every time it’s invoked
+
+function togglePrice() {
+  let prices = document.getElementsByClassName("price");
+  for (let el of prices) {
+    el.addEventListener("click", function () {
+      el.style.color = rcg();
+    });
+  }
+}
+
+togglePrice();
